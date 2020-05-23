81ad80a4cff99cb3ed048800538486045adbcb9f
Robot1986 <blr101@mail.ru>
2020-05-23 19:26:21 +0300

    Complete task2 task3

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -204,8 +204,7 @@ function roundToPowerOfTen(num, pow) {
  */
 function isPrime(n) {
   for (let i = 2; i * i <= n; i++) {
-    if (n % i === 0)
-      return false;
+    if (n % i === 0) return false;
   }
   return true;
 }
